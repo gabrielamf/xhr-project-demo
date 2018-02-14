a68468ade46fbea387f1330f398825f699ae30f2
gabrielamf <glennygabb@gmail.com>
2018-02-14 01:51:59 -0500

    añade boton submit fetch

---
+++ README.md
@@ -0,0 +1,18 @@
+# HHR Project Demo
+
+**Curso:** _Construye una SINGLE PAGE APP (SPA) multi-usuario consumiendo data remota_  
+**Unidad:** _¿Qué hay de nuevo en HTML5?_
+
+## Objetivos
+Usar la api de nytimes para implementar una página web de búsquedas de articulos sobre un tema en especial.
+
+## Resultado
+![vista](public/assets/docs/xhr-demo.png)
+
+
+## Desarrollado con:
+
+`HTML5` `CSS3` `Materialize` `API-nyTimes`
+
+## Desarrollado para 
+[Laboratoria](http://laboratoria.la)
--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,49 @@
-const form = document.getElementById('search-form');
+// const form = document.getElementById('search-form');
 const searchField = document.getElementById('search-keyword');
 const responseContainer = document.getElementById('response-container');
+const btnHxr = document.getElementById('xhr-btn');
+const btnFetch = document.getElementById('fetch-btn');
 let searchedForText;
 
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
+btnHxr.addEventListener('click', function(event) {
+  event.preventDefault();
   responseContainer.innerHTML = '';
   searchedForText = searchField.value;
   getNews();
 });
 
+btnFetch.addEventListener('click', function(e) {
+  e.preventDefault();
+  responseContainer.innerHTML = '';
+  searchedForText = searchField.value;
+  let uri = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=9b862bfc87dc4ac89e968cad79b16cbc`;
+  fetch(uri)
+    .then(function(response) {
+      return response.json();
+    }).then(function(data) {
+      const response = data.response.docs;
+      addNews(response);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
+
 function getNews() {
   const articleRequest = new XMLHttpRequest();
-  articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=9b862bfc87dc4ac89e968cad79b16cbc`);
-  articleRequest.onload = addNews;
-  articleRequest.onerror = handleError;
+  articleRequest.onreadystatechange = function() {
+    if (articleRequest.readyState === 4 && articleRequest.status === 200) {
+      const data = JSON.parse(this.responseText);
+      const response = data.response.docs;
+      articleRequest.onload = addNews(response);
+      articleRequest.onerror = handleError; 
+    } 
+  };
+  articleRequest.open('GET', `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=9b862bfc87dc4ac89e968cad79b16cbc`);
   articleRequest.send();
 }
 
-function handleError() {
-  console.log('Se ha presentado un error');
-}
-
-function addNews() {
-  const data = JSON.parse(this.responseText); // cuando pongo solo response me da lo mismo ¿?
-  const response = data.response;
-  const article = data.response.docs; //
-
+function addNews(article) {
   article.map((article) => {
     console.log(article);
     const title = article.headline.main;
@@ -56,5 +73,9 @@ function addNews() {
   /* console.log(this.responseText);
   console.log(data.response);
   console.log(this.responseText.docs);*/
+}
+
+function handleError(error) {
+  console.log(error);
 }
 
